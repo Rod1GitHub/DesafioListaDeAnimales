769f801cb6891493cee1c1416d9aa450e13db8b4
Rod <rod@Rods-MacBook-Pro.local>
2021-12-22 13:19:52 -0300

    Array funcionando

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,29 +5,8 @@ class Propietario {
         this._direccion = direccion;
         this._telefono = telefono;
     }
-    
-    get Name(){
-        return this._nombre;
-    }
-    get Address(){
-        return this._direccion;
-    }
-    get Phone(){
-        return this._telefono;
-    }
-
-    set Name(newNombre){
-    this._nombre = newNombre;
-    }
-    set Address(newDireccion){
-        this._direccion = newDireccion;
-    }
-    set Phone(newTelefono){
-        this._telefono = newTelefono;
-    }
-
-    infoPropietario() {
-        return `El nombre del propietario es: ${this._nombre}. El domicilio es: ${this._direccion}, y el número telefónico de contacto: ${this._telefono}`;
+    datosPropietario() {
+        return `El nombre del dueño es: ${this._nombre}. El domicilio es: ${this._direccion}, y el número telefónico de contacto: ${this._telefono}`;
     }
 }
 
@@ -37,15 +16,11 @@ class Animal extends Propietario {
         super(nombre, direccion, telefono)
         this._tipo = tipo
     }
+
     get Tipo(){
         return this._tipo
     }
-    set Tipo(newTipo){
-        this._tipo = newTipo
-    }
-    infoAnimal() {
-        return `El nombre del propietario es: ${this._nombre}. El domicilio es: ${this._direccion}, y el número telefónico de contacto: ${this._telefono}`;
-    }
+
 }
 
 class Mascota extends Animal {
@@ -57,64 +32,52 @@ class Mascota extends Animal {
     get NombreMascota(){
         return this._nombreMascota
     }
-    get Enfermedad(){
-        return this._enfermedad
-    }
-
     set NombreMascota(newNombreMascota){
         this._nombreMascota = newNombreMascota
     }
-
+    get Enfermedad(){
+        return this._enfermedad
+    }
     set Enfermedad(newEnfermedad){
         this._enfermedad = newEnfermedad
     }
-    infoMascota() {
-        return `mientras que el nombre de la mascota es: ${this.nombreMascota} y la enfermedad es: ${this.enfermedad}`;
-    }
+
 
 }
 
-// OK hasta aquí
-/* 
 document.querySelector('form').addEventListener('submit',(e)=>{
     e.preventDefault();
 
 // asignar input a las variables
     let propietario = document.querySelector('#propietario').value;
+    propietario.innerHTML = ``;
     let telefono = document.querySelector('#telefono').value;
+    telefono.innerHTML = ``;
     let direccion = document.querySelector('#direccion').value;
+    direccion.innerHTML = ``;
     let nombreMascota = document.querySelector('#nombreMascota').value;
+    nombreMascota.innerHTML = ``;
     let tipo = document.querySelector('#tipo').value;
+    tipo.innerHTML = ``;
     let enfermedad = document.querySelector('#enfermedad').value;
-    //let resultado = document.querySelector('#resultado').value;
+    enfermedad.innerHTML = ``;
 
-// limpiar
-propietario.innerHTML = ``;
-telefono.innerHTML = ``;
-direccion.innerHTML = ``;
-nombreMascota.innerHTML = ``;
-//tipo.innerHTML = ``;
-enfermedad.innerHTML = ``;
-//resultado.innerHTML = ``;
+    console.log(propietario);
+    console.log(telefono);
+    console.log(direccion);
+    console.log(nombreMascota);
+    console.log(tipo);
+    console.log(enfermedad);
 
-// OK hasta aquí
+    let datosIngresados = new Object([propietario, telefono, direccion, nombreMascota, tipo, enfermedad]);
+    
+    console.log(datosIngresados)
+
+    document.getElementById("resultado").innerHTML = datosIngresados;
+    });
 
 
-const obtenerValoresInputs = (valueIdNameElement) => {
-    return document.getElementById(valueIdNameElement).value;
-} 
 
-    const objMascota = new Mascota(propietario, direccion, telefono, tipo, nombreMascota, enfermedad);
 
-    const resultElementUl = document.getElementById('resultado').children[0];
 
-    const liDatosPropetario = document.createElement("li");
-    const liTipoNombreEnfermedadMascota = document.createElement("li");
-
-    liDatosPropetario.innerHTML = `${objMascota.datosPropietarios()}`;
-    liTipoNombreEnfermedadMascota.innerHTML = `${objMascota.getTipo()} ${objMascota.getNombreEnfermedadMascota()}`;
-
-    resultElementUl.appendChild(liDatosPropetario);
-    resultElementUl.appendChild(liTipoNombreEnfermedadMascota);
-});
- */
+  //  “El tipo de animal es un: ${this.tipo}”, mientras que el nombre de la mascota es Tatty y la enfermedad es: otitis leve
